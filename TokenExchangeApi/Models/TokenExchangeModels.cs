9bbf24ed96c047173bfe9e39a34ba9b2c7588c6d
Wilson Bautista <wilibac@hotmail.com>
2026-08-18 08:39:19 -0500

    Add token exchange request/response models

---
+++ TokenExchangeApi/Models/TokenExchangeModels.cs
@@ -0,0 +1,25 @@
+﻿namespace TokenExchangeApi.Models;
+
+public sealed class TokenExchangeRequest
+{
+    public required string GrantType { get; init; }
+    public required string SubjectToken { get; init; }
+    public required string SubjectTokenType { get; init; }
+    public required string Audience { get; init; }
+    public required string Scope { get; init; }
+}
+
+public sealed class TokenExchangeResponse
+{
+    public required string AccessToken { get; init; }
+    public string IssuedTokenType { get; init; } = "urn:ietf:params:oauth:token-type:jwt";
+    public string TokenType { get; init; } = "Bearer";
+    public required int ExpiresIn { get; init; }
+    public required string Scope { get; init; }
+}
+
+public sealed class TokenExchangeError
+{
+    public required string Error { get; init; }
+    public required string ErrorDescription { get; init; }
+}
